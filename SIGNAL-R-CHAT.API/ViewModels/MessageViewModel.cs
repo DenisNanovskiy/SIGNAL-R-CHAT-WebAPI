56cefb54c93bcf0527aeeb7a678f7b4c1b4adeb6
DenisNanovskiy <56844278+DenisNanovskiy@users.noreply.github.com>
2022-01-06 08:34:28 +0300

    Add SignalR Chathub

---
+++ SIGNAL-R-CHAT.API/ViewModels/MessageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIGNAL_R_CHAT.API.ViewModels
+{
+    public class MessageViewModel
+    {
+        public string Text { get; set; }
+        public string DateTime { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
---
+++ SIGNAL-R-CHAT.API/ViewModels/PersonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIGNAL_R_CHAT.API.ViewModels
+{
+    public class PersonViewModel
+    {
+        public string PersonName { get; set; }
+        public string CurrentWorkGroup { get; set; }
+    }
+}
